a2f743bf80a1a4c4a24c8b201778e1c3197cfe5d
Albert DeFusco <albert.defusco@me.com>
2018-06-28 19:40:59 -0400

    add script path
    run_path does not allow the external script to
    import from it's local directory.
    
    I don't know if this fix is considered dangerous

--- tranquil/__main__.py
+++ tranquil/__main__.py
@@ -1,9 +1,14 @@
+import sys
+from os.path import dirname
+
 from .application import make_app, cli
 from .handler import get_tranquilized_functions
 
 
 def main():
     args = cli().parse_args()
+
+    sys.path.append(dirname(args.filename))
     functions = get_tranquilized_functions(args.filename)
     app = make_app(functions)
 
