100dc972f6a8e23fa289d5e22169b3e78ff73635
Albert DeFusco <albert.defusco@me.com>
2018-06-28 23:38:15 -0400

    longer docstring

--- examples/cheese_shop.py
+++ examples/cheese_shop.py
@@ -2,6 +2,8 @@ from tranquilizer import tranquilize
 
 @tranquilize()
 def order(cheese):
-    '''I'd like to buy some cheese!'''
+    '''I'd like to buy some cheese!
+
+    :param cheese: What cheese would you like?'''
     return {'response':"I'm afraid we're fresh out of {}, Sir.".format(cheese)}
 
