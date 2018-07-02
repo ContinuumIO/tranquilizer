dacf41de764c32472f1f894b377fb586558cff92
Albert DeFusco <albert.defusco@me.com>
2018-07-02 11:00:11 -0400

    post examples

--- examples/files.py
+++ examples/files.py
@@ -1,3 +1,13 @@
+# curl -X POST -F "file=@<text-file>" http://localhost:8086/text_file
+# requests.post('http://localhost:8086/text_file', files={'file':open('<text-file>', 'rb')})
+
+
+# curl -X POST -F "arr=@<array-file.npy>" http://localhost:8086/array_file
+# requests.post('http://localhost:8086/array_file', files={'arr':open('<np-array-file.npy>', 'rb')})
+# f = BytesIO()
+# np.save(f, arr)
+# requests.post('http://localhost:8086/array_file', files={'arr':f.getvalue()})
+
 from tranquilizer import tranquilize
 from tranquilizer.types import TextFile, NDArray
 import numpy as np
