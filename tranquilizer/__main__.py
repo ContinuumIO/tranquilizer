f4a4ab3f3ddd11c830c1ba1916c42401672e6435
Albert DeFusco <albert.defusco@me.com>
2019-01-14 09:17:58 -0500

    something that works
    can't run any magics and  they should be filtered out

--- tranquilizer/__main__.py
+++ tranquilizer/__main__.py
@@ -2,16 +2,25 @@ import sys
 from os.path import dirname, basename
 
 from .application import make_app, cli
-from .handler import get_tranquilized_functions
+from .handler import ScriptHandler, NotebookHandler
+
+class UnsupportedFileType(Exception):
+    pass
 
 def main():
     args = cli().parse_args()
     sys.path.append(dirname(args.filename))
 
-    functions = get_tranquilized_functions(args.filename)
+    extension = args.filename.split('.')[-1]
+    if extension == 'py':
+        source = ScriptHandler(args.filename)
+    elif extension == 'ipynb':
+        source = NotebookHandler(args.filename)
+    else:
+        raise UnsupportedFileType('{} is not a script (.py) or notebook (.ipynb)'.format(args.filename))
 
     name = args.name if args.name else basename(args.filename)
-    app = make_app(functions, name=name, prefix=args.anaconda_project_url_prefix)
+    app = make_app(source.tranquilized_functions, name=name, prefix=args.anaconda_project_url_prefix)
 
     app.run(host=args.anaconda_project_address, port=args.anaconda_project_port,
             debug=args.debug)
--- tranquilizer/handler.py
+++ tranquilizer/handler.py
@@ -1,30 +1,62 @@
 '''execute the script file'''
 import ast
 from runpy import run_path
-
-def _parse_script(fn):
-    with open(fn, 'r') as f:
-        source = f.read()
-
-    nodes = ast.parse(source, fn)
-    return nodes
-
+from abc import ABC, abstractmethod
+from os.path import dirname, join, basename
+import os
 
 def _is_tranquilized(decorator):
     return hasattr(decorator, 'id') and (decorator.id in ('tranquilize',))
-
 
 def _is_decorated(item):
     if isinstance(item, ast.FunctionDef):
         return any([_is_tranquilized(d.func) for d in item.decorator_list])
 
+class BaseHandler(object):
 
-def get_tranquilized_functions(fn):
-    nodes = _parse_script(fn)
-    tranquilized = [f.name for f in nodes.body if _is_decorated(f)]
+    def parse(self):
+        raise NotImplementedError()
 
-    module = run_path(fn)
-    funcs = [module[f] for f in tranquilized]
+    @property
+    def tranquilized_functions(self):
+        self.parse()
 
-    return funcs
+        tranquilized = [f.name for f in self.nodes.body if _is_decorated(f)]
+        functions = [self.module[f] for f in tranquilized]
+    
+        return functions
 
+
+class ScriptHandler(BaseHandler):
+    def __init__(self, fn):
+        self.fn = fn
+
+    def parse(self):
+        with open(self.fn, 'r') as f:
+            source = f.read()
+
+        self.nodes = ast.parse(source, self.fn)
+        self.module = run_path(self.fn)
+
+
+class NotebookHandler(BaseHandler):
+    def __init__(self, fn):
+        self.fn = fn
+    
+    def parse(self):
+        from nbconvert import ScriptExporter
+
+        exporter = ScriptExporter()
+        source, _ = exporter.from_filename(self.fn)
+
+        #maybe not the best idea, but works
+        directory = dirname(self.fn)
+        script_name = basename(self.fn) + '.py'
+        script_file = join(directory, script_name)
+        with open(script_file, 'w') as f:
+            f.write(source)
+
+        self.nodes = ast.parse(source, script_file)
+        self.module = run_path(script_file)
+
+        os.remove(script_file)
