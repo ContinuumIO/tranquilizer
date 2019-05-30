c2d333858ecb2d24e589256e3edbd02a4b219816
Albert DeFusco <albert.defusco@me.com>
2019-05-30 12:51:56 -0400

    3.7 not allowed yet

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: BSD License",
         "Framework :: Flask",
         "Operating System :: OS Independent",
@@ -48,7 +47,7 @@ setup(
         "Environment :: Web Environment",
         "Topic :: WWW/HTTP :: Dynamic Content"
     ],
-    python_requires=">=3.5",
+    python_requires=">=3.5, <3.7",
     install_requires=install_requires,
     extrax_require=extras_require,
     long_description=long_description
