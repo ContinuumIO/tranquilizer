3aa909d3d0f88d7ac0167cd62250ca4f91eb2846
Albert DeFusco <albert.defusco@me.com>
2019-08-29 15:08:37 -0400

    trying to fix proxyfix

--- tranquilizer/application.py
+++ tranquilizer/application.py
@@ -52,7 +52,10 @@ def make_app(functions, name, prefix='/', max_content_length=None):
     app.config['PREFERRED_URL_SCHEME'] = 'https'
     if max_content_length is not None:
         app.config['MAX_CONTENT_LENGTH'] = max_content_length
-    app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.wsgi_app = ProxyFix(app.wsgi_app,
+                            x_for=1, x_proto=1,
+                            x_host=1, x_port=1,
+                            x_prefix=1)
 
     @api.errorhandler
     def _default_error(error):
